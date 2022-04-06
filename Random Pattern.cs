72a6c46e8fe7140e0420b76eecd9b17961e09504
Muskan Sagar <99795889+Muskan1283@users.noreply.github.com>
2022-04-06 18:52:16 +0530

    Add files via upload

---
+++ Random Pattern.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Random_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 5;
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = i; j >= 1; j--)
+                {
+                    Random rd = new Random();
+                    int n = rd.Next(65, 90);
+                    Console.Write(Convert.ToChar(n) + "");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
+
